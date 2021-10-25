0e60bdd6fb23ba094952ea84c7453d0c852ca9c9
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-10-25 11:06:59 +0200

    Added ADB connection_urls for module outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
-## Copyright © 2020, Oracle and/or its affiliates. 
+## Copyright © 2021, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 output "adb_database" {
   value = {
     adb_database_id    = oci_database_autonomous_database.adb_database.id
+    connection_urls    = oci_database_autonomous_database.adb_database.connection_urls
     adb_wallet_content = oci_database_autonomous_database_wallet.adb_database_wallet.content
     adb_nsg_id         = (!var.use_existing_vcn && var.adb_private_endpoint) ? oci_core_network_security_group.adb_nsg[0].id : var.adb_nsg_id
   }
